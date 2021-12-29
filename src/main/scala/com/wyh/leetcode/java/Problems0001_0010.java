474937f582173cfd6fce6e002459ef9c5b8fcd94
AsYourWishes <18731040599@163.com>
2021-12-29 22:31:20 +0800

    problem 0002

--- src/main/scala/com/wyh/leetcode/java/Problems0001_0010.java
+++ src/main/scala/com/wyh/leetcode/java/Problems0001_0010.java
@@ -26,4 +26,43 @@ class Problems0001_0010 {
         return new int[0];
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode head = null;
+        ListNode tail = null;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null){
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            if (head == null){
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null){
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0){
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
 }
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
